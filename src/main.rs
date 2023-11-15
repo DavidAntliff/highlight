7e8b482502bd1e4cba6234c22cc5d102b8de9da2
David Antliff <d.antliff@unsw.edu.au>
2023-11-16 10:40:44 +1300

    Refactoring.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 use regex::Regex;
 use std::fs;
 use std::io::{self, BufRead};
+use anyhow::{anyhow, Result};
 
 /// Command Line Argument Parser for Rust
 #[derive(Parser, Debug)]
@@ -22,28 +23,31 @@ struct Args {
     files: Vec<String>,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
 
-    let regex = Regex::new(&args.pattern).expect("Invalid regex pattern");
+    let regex = Regex::new(&args.pattern)?;
+
+    let code = get_format_code(&args.colour, args.bold)?;
 
     if args.files.is_empty() {
         let stdin = io::stdin();
         for line in stdin.lock().lines() {
             let line = line.expect("Could not read line");
-            print_highlighted(&regex, &line, &args.colour, args.bold);
+            print_highlighted(&regex, &line, &code);
         }
     } else {
         for file in args.files {
             let content = fs::read_to_string(&file).expect("Could not read file");
-            print_highlighted(&regex, &content, &args.colour, args.bold);
+            print_highlighted(&regex, &content, &code);
         }
     }
+
+    Ok(())
 }
 
-fn print_highlighted(regex: &Regex, text: &str, colour: &str, bold: bool) {
-
-    let color_code = match colour {
+fn get_format_code(colour: &str, bold: bool) -> Result<String> {
+    let colour_code = match colour {
         "red" => "\x1b[31m",
         "green" => "\x1b[32m",
         "blue" => "\x1b[34m",
@@ -51,12 +55,18 @@ fn print_highlighted(regex: &Regex, text: &str, colour: &str, bold: bool) {
         "cyan" => "\x1b[36m",
         "magenta" => "\x1b[35m",
         "white" => "\x1b[37m",
-        _ => "\x1b[0m",
+        _ => {
+            Err(anyhow!("Unexpected colour '{}'", colour))?
+        },
     };
 
     //let (r, g, b) = (255, 0, 255);
     //let color_code = format!("\x1b[38;2;{};{};{}m", r, g, b);
     let bold_code = if bold { "\x1b[1m" } else { "" };
-    let highlighted_text = regex.replace_all(text, format!("{}{}$0\x1b[0m", bold_code, color_code));
+    Ok(format!("{}{}", bold_code, colour_code))
+}
+
+fn print_highlighted(regex: &Regex, text: &str, code: &str) {
+    let highlighted_text = regex.replace_all(text, format!("{}$0\x1b[0m", code));
     println!("{}", highlighted_text);
 }
