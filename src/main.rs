bb9eb082ac9630d6b7464b17940418b893419511
David Antliff <d.antliff@unsw.edu.au>
2023-11-16 10:24:04 +1300

    Update to use Clap 4.

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,47 @@
-use clap::{App, Arg};
+use clap::Parser;
 use regex::Regex;
 use std::fs;
 use std::io::{self, BufRead};
 
+/// Command Line Argument Parser for Rust
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// The pattern to highlight
+    pattern: String,
+
+    /// Colour for highlighting (red, green, blue, yellow, cyan, magenta, white)
+    #[clap(short, long, default_value = "red")]
+    colour: String,
+
+    /// Make the highlighting bold
+    #[clap(short, long)]
+    bold: bool,
+
+    /// Files to search
+    files: Vec<String>,
+}
+
 fn main() {
-    let matches = App::new("highlight")
-        .version("0.1")
-        .about("Highlights patterns in text")
-        .arg(
-            Arg::new("pattern")
-                .help("The pattern to highlight")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::new("files")
-                .help("Files to search")
-                .multiple_values(true)
-                .index(2),
-        )
-        .arg(
-            Arg::new("colour")
-                .short('c')
-                .long("colour")
-                .takes_value(true)
-                .help("Colour for highlighting (red, green, blue, yellow, cyan, magenta, white)"),
-        )
-        .arg(
-            Arg::new("bold")
-                .short('b')
-                .long("bold")
-                .help("Make the highlighting bold"),
-        )
-        .get_matches();
+    let args = Args::parse();
 
-    let pattern = matches.value_of("pattern").unwrap();
-    let colour = matches.value_of("colour").unwrap_or("red");
-    let bold = matches.is_present("bold");
-    let regex = Regex::new(pattern).expect("Invalid regex pattern");
+    let regex = Regex::new(&args.pattern).expect("Invalid regex pattern");
 
-    match matches.values_of("files") {
-        Some(files) => {
-            for file in files {
-                let content = fs::read_to_string(file).expect("Could not read file");
-                print_highlighted(&regex, &content, colour, bold);
-            }
+    if args.files.is_empty() {
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            let line = line.expect("Could not read line");
+            print_highlighted(&regex, &line, &args.colour, args.bold);
         }
-        None => {
-            let stdin = io::stdin();
-            for line in stdin.lock().lines() {
-                let line = line.expect("Could not read line");
-                print_highlighted(&regex, &line, colour, bold);
-            }
+    } else {
+        for file in args.files {
+            let content = fs::read_to_string(&file).expect("Could not read file");
+            print_highlighted(&regex, &content, &args.colour, args.bold);
         }
     }
 }
 
 fn print_highlighted(regex: &Regex, text: &str, colour: &str, bold: bool) {
-    let bold_code = if bold { "\x1b[1m" } else { "" };
 
     let color_code = match colour {
         "red" => "\x1b[31m",
@@ -72,7 +56,7 @@ fn print_highlighted(regex: &Regex, text: &str, colour: &str, bold: bool) {
 
     //let (r, g, b) = (255, 0, 255);
     //let color_code = format!("\x1b[38;2;{};{};{}m", r, g, b);
-
+    let bold_code = if bold { "\x1b[1m" } else { "" };
     let highlighted_text = regex.replace_all(text, format!("{}{}$0\x1b[0m", bold_code, color_code));
     println!("{}", highlighted_text);
 }
